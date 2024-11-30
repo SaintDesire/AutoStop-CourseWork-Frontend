8e92e149b51057a7bafd9bdd4cd7780f1394921b
Alexandr Korshun <nikitakorshun@mail.ru>
2024-11-30 12:54:17 +0300

    todo problem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import Login from "./auth/login/page";
 import { DASHBOARD_PAGES } from "@/config/pages-url.config";
 
 export default function Home() {
--- src/components/ui/header.jsx
+++ src/components/ui/header.jsx
@@ -17,6 +17,7 @@ const user = {
     "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 };
 
+// Исправить проблему с обновлением страницы и изменением цвета кнопки текущей страницы
 const initialNavigation = [
   { name: "Главная", href: DASHBOARD_PAGES.HOME, current: true },
   { name: "Каталог", href: DASHBOARD_PAGES.CARLIST, current: false },
